b28d4da41210bf80500a5bb7a30241476dc12c49
Kundan Nanote <kundan055@hotmail.com>
2021-06-01 23:19:56 +0530

    Submit Changes

---
+++ ContactInformationCore.WebAPI/ContactService.cs
@@ -0,0 +1,119 @@
+﻿using ContactInformationCore.Interface;
+using ContactInformationCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContactInformationCore.WebAPI
+{
+    public class ContactService : IContact
+    {
+        readonly DatabaseContaxt _dataContext;
+
+        public ContactService(DatabaseContaxt context)
+        {
+            _dataContext = context;
+        }
+
+
+        public void SaveContact(Contact Contact)
+        {
+            _dataContext.Contacts.Add(Contact);
+            _dataContext.SaveChanges();
+        }
+
+        public int DeleteContact(int? id)
+        {
+            Contact contact = _dataContext.Contacts.Find(id);
+            _dataContext.Contacts.Remove(contact);
+            return _dataContext.SaveChanges();
+        }
+
+        public Contact ContactByID(int? id)
+        {
+            try
+            {
+                return _dataContext.Contacts.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void UpdateContact(Contact ContacttoUpdate, Contact Contact)
+        {
+            try
+            { 
+                if (ContacttoUpdate != null)
+                {
+                    Contact.First_Name = ContacttoUpdate.First_Name;
+                    Contact.Last_Name = ContacttoUpdate.Last_Name;
+                    Contact.Email = ContacttoUpdate.Email;
+                    Contact.Phone_Number = ContacttoUpdate.Phone_Number;
+                    Contact.Status = ContacttoUpdate.Status;
+
+                    //Commit the transaction
+                    _dataContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool CheckEmailIsAlreadyExist(string Email)
+        {
+            var EmailCount = (from email in _dataContext.Contacts
+                              where email.Email == Email
+                              select email).Count();
+
+            if (EmailCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Contact> GetAllContact()
+        {
+            var ContactList = (from contact in _dataContext.Contacts
+                               select new Contact
+                               {
+                                   Id = contact.Id,
+                                   First_Name = contact.First_Name,
+                                   Last_Name = contact.Last_Name,
+                                   Email = contact.Email,
+                                   Phone_Number = contact.Phone_Number,
+                                   Status = contact.Status
+                               }).ToList();
+
+            return ContactList;
+        }
+
+        public IEnumerable<Contact> ShowAllContact()
+        {
+
+            var ContactList = (from contact in _dataContext.Contacts
+                               select new Contact
+                               {
+                                   Id = contact.Id,
+                                   First_Name = contact.First_Name,
+                                   Last_Name = contact.Last_Name,
+                                   Email = contact.Email,
+                                   Phone_Number = contact.Phone_Number,
+                                   Status = contact.Status
+                               }).ToList();
+
+            return ContactList;
+        }
+
+        
+
+    }
+}
---
+++ ContactInformationCore.WebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,145 @@
+﻿using ContactInformationCore.Interface;
+using ContactInformationCore.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContactInformationCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private IContact _IContact;
+
+        public ContactsController(IContact IContact)
+        {
+            _IContact = IContact;
+        }
+
+        // GET: api/Contacts
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Contact> contact = _IContact.GetAllContact();
+            return Ok(contact);
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int? id)
+        {
+            try
+            {
+                if(id == null) { return BadRequest(); }
+
+                Contact contact = _IContact.ContactByID(id);
+
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(contact);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        // PUT: api/Contacts/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int? id, [FromBody] Contact contactToUpdate)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (contactToUpdate == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    Contact contact = _IContact.ContactByID(id);
+
+                    if (contactToUpdate == null)
+                    {
+                        return NotFound();
+                    }
+
+                    _IContact.UpdateContact(contactToUpdate, contact);
+                    return Ok();
+                }
+                catch (Exception ex)
+                {
+                    if (ex.GetType().FullName == "Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException")
+                    {
+                        return NotFound();
+                    }
+
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+
+        // POST: api/Skills
+        [HttpPost]
+        public IActionResult Post([FromBody] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (contact == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    _IContact.SaveContact(contact);
+
+                    if (contact.Id > 0)
+                        return CreatedAtRoute("Get", new { Id = contact.Id }, contact);
+                    else
+                        return NotFound();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+
+        // DELETE: api/Skills/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int? id)
+        {
+            int result = 0;
+            try
+            {
+                if (id == null) { return BadRequest(); }
+
+                Contact contact = _IContact.ContactByID(id);
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+
+                result = _IContact.DeleteContact(id);
+                if (result == 0)
+                {
+                    return BadRequest();
+                }
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
